7c4bb7b1a771f4ca06617dd4cbb9afad62563f80
Deltuh <deltfps@gmail.com>
2022-01-16 08:01:08 -0500

    Bull's Eye App

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,12 +8,117 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var roundLabel: UILabel!
+    
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    //Converting an Int into a String to pass the label's text property
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewGame()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighligted = UIImage(
+            named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighligted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(
+            top: 0,
+            left: 14,
+            bottom: 0,
+            right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(
+            withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(
+            withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+                                                            
+    }
+    
+    // Restarts the game
+    @IBAction func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+        
+        let transition = CATransition()
+        transition.type = CATransitionType.fade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(
+            name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
     }
 
-
+    @IBAction func showAlert() {
+        
+        //Calculating the differnece
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        
+        //Giving the player some sort of feedback when scoring
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+                points += 100                    // If player gets a perfect score, bonus 100 points
+        }   else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50                     // If player is 1 point off, bonus 50 points
+            }
+        }   else if difference < 10 {
+            title = "Pretty good!"
+        }   else {
+            title = "Not even close..."
+        }
+        score += points
+        
+        let message = "You scored \(points) points"
+    
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default) {_ in
+            self.startNewRound()
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 }
+
 
